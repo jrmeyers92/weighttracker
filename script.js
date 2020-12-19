99dfad390a87cdbb98d832b5906087d80d563b5f
Jake Meyers <jakejake92@yahoo.com>
2020-12-18 17:36:29 -0700

    progress on BMI functions

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// create an array to hold all user objects
+const users = [];
+//variables for create user form
+const name = document.getElementById("newUser__name");
+const age = document.getElementById("newUser__age");
+const heightFeet = document.getElementById("form__feet");
+const heightInches = document.getElementById("form__inches");
+const currentWeight = document.getElementById("newUser__currentWeight");
+const goalWeight = document.getElementById("newUser__goalWeight");
+// getting lenght off users array in order to uniqly name new users
+let arrayLength = users.length;
+
+let BMI;
+
 // function constructor to create new user instances
 
 function User(
@@ -22,12 +36,34 @@ function User(
 	this.goalDate = goalDate;
 }
 
+const calculateMeters = (feet, inches) => {
+	//convert weight inputs to numbers
+	let feetInt = parseInt(feet);
+	let inchesInt = parseInt(inches);
+	//convert ft to inches
+	let convertedInches = feetInt * 12;
+	let totalInches = inchesInt + convertedInches;
+	let meters = totalInches * 0.0254;
+	return meters;
+};
+
+const calculateKGsSquared = (weight) => {
+	//conver weight to kg's
+	let KGs = weight * 0.453592;
+	KGsInt = parseInt(KGs);
+	kGsSquared = Math.pow(KGsInt, 2);
+	return kGsSquared;
+};
+
+//function to calculate BMI
+const calculateBMI = (weight, feet, inches) => {
+	BMI = calculateMeters(feet, inches) / calculateKGsSquared(weight);
+	console.log(BMI);
+};
+
 // event listener for when new user form is submited
 const newUserForm = document.getElementById("newUser__form");
-newUserForm.addEventListener("submit", () => {
-	const name = document.getElementById("newUser__name");
-	const age = document.getElementById("newUser__age");
-	const height = document.getElementById("newUser__age");
-	const weight = document.getElementById("newUser__age");
-	const goalWeight = document.getElementById("newUser__age");
+newUserForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+	calculateBMI(currentWeight.value, heightFeet.value, heightInches.value);
 });
